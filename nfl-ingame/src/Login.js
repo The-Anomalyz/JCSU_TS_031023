b23f29ed3d3fae39f1e02b0e9c9fe839f80f2b4e
Jkakwenya <93216139+Jkakwenya@users.noreply.github.com>
2023-03-09 16:22:44 -0600

    Finished Authentication

--- nfl-ingame/src/Login.js
+++ nfl-ingame/src/Login.js
@@ -4,16 +4,25 @@
 import './Login.css'
 import { useNavigate } from 'react-router-dom';
 import React, {useState} from 'react'
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from './firebase'
+import {  signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
     const navigate = useNavigate();
-    const auth = getAuth();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+      };
+    
+      const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+      };
+
+
     const login = e => {
         e.preventDefault();
-    signInWithEmailAndPassword( auth, email, password).then((auth) => {
+        signInWithEmailAndPassword( auth, email, password).then((auth) => {
       if (auth){
           navigate('/')
         }
@@ -25,19 +34,9 @@ function Login() {
         <h1>Login Page</h1>
         <form >
           <h5>Email</h5>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            required
-          />
+          <input type="email" value={email} onChange={handleEmailChange}/>
           <h5>Password:</h5>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            required
-          />
+          <input type="password" value={password} onChange={handlePasswordChange}/>
             <button className='login-button' type="submit" onClick={login}>Login</button>
             <button className='login-button'>Forgot Password</button>
             <button className='login-button-1' >Sign in with Google</button>
