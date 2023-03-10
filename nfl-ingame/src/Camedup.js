ff13113c64ba39663517a429b644bdaece70e44f
MuchCodin <muchiemombeshora@gmail.com>
2023-03-10 04:48:25 -0600

    Adding CamdUp page

--- nfl-ingame/src/Camedup.js
+++ nfl-ingame/src/Camedup.js
@@ -4,7 +4,12 @@ import './Camedup.css'
 function Camedup() {
   return (
     <div>
-      
+      <h1>Dive into exclusive content</h1>
+      <img src="camdupexclusive.jpeg" alt="CamdUp" style={{ maxWidth: '400px', maxHeight: '200px',
+            width: '320px',
+            height: '300px', padding: '10px',
+          }
+        }/>
     </div>
   )
 }
