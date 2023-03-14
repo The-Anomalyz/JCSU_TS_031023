37e00d878a028bf3701834f8a5bff3ef29ab3bac
Jkakwenya <93216139+Jkakwenya@users.noreply.github.com>
2023-03-13 21:47:04 -0400

    Implemented the logout

--- nfl-ingame/src/Header.js
+++ nfl-ingame/src/Header.js
@@ -1,11 +1,28 @@
 
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import './Header.css'
+import { getAuth, signOut } from "firebase/auth";
 
 function Header() {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+    const handleSignOut = () => {
+      setIsLoggingOut(true);
+      
+      if (user) {signOut(auth).then(() => {
+      // Sign-out successful.
+      console.log("Sign-out successful.");
+    }).catch((error) => {
+      // An error happened.
+      console.error("An error happened: ", error);
+    }).finally(() => {
+      setIsLoggingOut(false);
+    });}
+  }
   const navigate = useNavigate();
   const handleClick = () => {
     navigate('/')
@@ -16,7 +33,13 @@ function Header() {
         <img onClick={handleClick} className='header-logo' src='sideline-PhotoRoom.png' alt=''/>
         <div className='header-nav'>
             <div className='header-nav-1'><Link to= '/' className='header-link'><span>Home</span></Link></div>
-            <div className='header-nav-1'><Link to='/login' className='header-link'><span>Login</span></Link></div>
+            <div className='header-nav-1'>
+          {isLoggingOut ? (
+            <span>Loading...</span>
+          ) : (
+            <Link to={!user ? '/login' : '/'} className='header-link'><span onClick={handleSignOut}>{user ? 'Logout' : 'Login'}</span></Link>
+          )}
+        </div>
             <div className='header-nav-1'><Link to='/profile' className='header-link'><span>Profile</span></Link></div>
             <div className='header-nav-account'>
               <div className='header-nav-1 header-nav-account-btn'>
